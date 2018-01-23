54c90dc1b77eb869c7fb60b93e30df13e65c2abd
Quentin Doyonnas <qdoyonnas@gmail.com>
2018-01-23 14:27:05 -0500

    Initial App Structure

---
+++ main.js
@@ -0,0 +1,10 @@
+var express = require("express");
+
+var app = express();
+var port = 3000;
+
+app.use(express.static("public/html")));
+
+app.listen(port, function(error) {
+	console.log("Server is running on port " + port);
+});
