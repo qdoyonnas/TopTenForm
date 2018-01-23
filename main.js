8c03a236acf05755dedef5976cc8b3691d023f96
Quentin Doyonnas <qdoyonnas@gmail.com>
2018-01-23 15:15:47 -0500

    Added core html and css

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var express = require("express");
 var app = express();
 var port = 3000;
 
-app.use(express.static("public/html")));
+app.use(express.static("public"));
+app.use(express.static("public/html"));
 
 app.listen(port, function(error) {
 	console.log("Server is running on port " + port);
