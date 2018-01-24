d802eaa05a8a65c72ecf05ed955c8232c5d04364
Doyonnas <qjdoyonnas@labs.neit.edu>
2018-01-24 13:06:20 -0500

    Switched to template format

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
+var http = require("http");
+var path = require("path");
 var express = require("express");
+var logger = require("morgan");
+var bodyParser = require("body-parser");
 
 var app = express();
 var port = 3000;
 
-app.use(express.static("public"));
-app.use(express.static("public/html"));
+app.set('views', path.resolve(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
 
-app.listen(port, function(error) {
-	console.log("Server is running on port " + port);
+var entries = [];
+app.locals.entries = entries;
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", function(request, response) {
+	response.render("index");
+});
+app.get("/new-entry", function(request, response) {
+	response.render("new-entry");
+});
+
+app.post("/new-entry", function(request, response) {
+	if( !request.body.title || !request.body.body ) {
+		response.status(400).send("Entries must have some text");
+		return;
+	}
+	
+	console.log(request);
+	
+	entries.push({
+		title: request.body.title,
+		body: request.body.body,
+		published: new Date()
+	});
+	response.redirect("/");
+});
+
+app.use(function(request, response) {
+	response.status(404).render("404");
+});
+
+http.createServer(app).listen(port, function() {
+	console.log("Server listening on port " + port);
 });
