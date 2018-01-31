63c59d21af5c77c79bdeed8c0ef77f6f8cb4bf03
Doyonnas <qjdoyonnas@labs.neit.edu>
2018-01-31 13:13:17 -0500

    Mongodb implementation

--- main.js
+++ main.js
@@ -3,21 +3,32 @@ var path = require("path");
 var express = require("express");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
+var mongoClient = require("mongodb");
 
+var url = "mongodb://localhost:27017/";
 var app = express();
 var port = 3000;
+mongoClient.connect(url);
 
 app.set('views', path.resolve(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
 
-var entries = [];
-app.locals.entries = entries;
+
 
 app.use(logger("dev"));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get("/", function(request, response) {
-	response.render("index");
+	mongoClient.connect(url, function(error, mongodb) {
+		if( error ) { throw error; }
+		
+		var db = mongodb.db("languages");
+		db.collection("languages").find().toArray(function(error, results) {
+			if( error ) { throw error; }
+			mongodb.close();
+			response.render("index", {entries: results});
+		});
+	});
 });
 app.get("/new-entry", function(request, response) {
 	response.render("new-entry");
@@ -29,12 +40,24 @@ app.post("/new-entry", function(request, response) {
 		return;
 	}
 	
-	entries.push({
+	mongoClient.connect(url, function(error, mongodb) {
+		if( error ) { throw error; }
+		
+		var db = mongodb.db("languages");
+		db.collection("languages").save(request.body, function(error, result) {
+			if( error ) { throw error; }
+			console.log("data saved");
+			mongodb.close();
+			response.redirect("/");
+		});
+	});
+	
+	/*entries.push({
 		title: request.body.title,
 		body: request.body.body,
 		published: new Date()
 	});
-	response.redirect("/");
+	response.redirect("/");*/
 });
 
 app.use(function(request, response) {
